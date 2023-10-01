cd09f39f641b0299976db87da4bcb28c27ba499e
DitGrau <juditgraup@gmail.com>
2023-10-01 17:01:03 +0200

    feat: transaction form

--- reactJournal-01/src/components/TransactionForm.jsx
+++ reactJournal-01/src/components/TransactionForm.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useGlobalState } from "../context/GlobalState";
 
-function TransactionForm(){
-    return(
-        <div>Transaction Form</div>
+
+function TransactionForm() {
+    const { addTransaction } = useGlobalState();
+    const [description, setDescription] = useState()
+    const [amount, setAmount] = useState()
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        addTransaction({id:1, description, amount})
+        console.log(description, amount)
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="Enter a description" onChange={(e) => setDescription(e.target.value)} />
+                <input type="number" step="0.01" placeholder="00.00" onChange={(e) => setAmount(e.target.value)} />
+                <button>Add Transaction</button>
+            </form>
+        </div>
     )
 }
 
