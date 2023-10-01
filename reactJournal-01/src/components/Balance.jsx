ce721cf39d5d2bfa2cbce840905bd08effa3d50a
DitGrau <juditgraup@gmail.com>
2023-10-01 14:25:02 +0200

    feat: custom hook use Global State, from context

--- reactJournal-01/src/components/Balance.jsx
+++ reactJournal-01/src/components/Balance.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Context } from "../context/GlobalState";
-import { useContext } from "react";
-
+import { useGlobalState } from "../context/GlobalState";
 
 function Balance() {
 
-    const data = useContext(Context)
+    const data = useGlobalState();
     return (
         <div>
             <h1>Balance</h1>
--- reactJournal-01/src/context/GlobalState.jsx
+++ reactJournal-01/src/context/GlobalState.jsx
@@ -1,6 +1,11 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 export const Context = createContext()
+
+export const useGlobalState = () => {
+    const context = useContext(Context)
+    return context 
+}
 
 export const GlobalProvider = ({ children }) => {
     return (
