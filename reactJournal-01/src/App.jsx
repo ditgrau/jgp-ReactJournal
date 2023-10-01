a53da35e50dcf4491afbefe763b9658ccd2a834c
DitGrau <juditgraup@gmail.com>
2023-10-01 14:18:12 +0200

    feat: global state defined, context

--- reactJournal-01/src/App.jsx
+++ reactJournal-01/src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import { GlobalProvider } from './context/GlobalState'
+import Header from './components/Header'
+import Balance from './components/Balance'
+import TransactionForm from './components/TransactionForm'
 
 
 function App() {
 
   return (
     <>
-      <div>Hello world</div>
+      <GlobalProvider>
+        <Header/>
+        <Balance></Balance>
+        <TransactionForm>
+          
+        </TransactionForm>
+        <h1>Hello world</h1>
+      </GlobalProvider>
     </>
   )
 }
---
+++ reactJournal-01/src/components/TransactionForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TransactionForm(){
+    return(
+        <div>Transaction Form</div>
+    )
+}
+
+export default TransactionForm
