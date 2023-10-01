ce20aa3b8412c2f7cd3f01767f71154e9e333c34
DitGrau <juditgraup@gmail.com>
2023-10-01 14:30:03 +0200

    feat: set context value from useState

--- reactJournal-01/src/context/GlobalState.jsx
+++ reactJournal-01/src/context/GlobalState.jsx
@@ -1,15 +1,18 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const Context = createContext()
 
 export const useGlobalState = () => {
     const context = useContext(Context)
-    return context 
+    return context
 }
 
 export const GlobalProvider = ({ children }) => {
+
+    const [state, setState] = useState([])
+
     return (
-        <Context.Provider value={{ total: 100 }}>
+        <Context.Provider value={{state, setState}}>
             {children}
         </Context.Provider>
     )
