7727784a7da675dcda6946c83328e8b5a14acc89
DitGrau <juditgraup@gmail.com>
2023-10-04 11:58:54 +0200

    "feat: clean form after add transaction"

--- reactJournal-01/src/components/transactions/TransactionForm.jsx
+++ reactJournal-01/src/components/transactions/TransactionForm.jsx
@@ -4,24 +4,31 @@ import { useGlobalState } from "../../context/GlobalState";
 
 function TransactionForm() {
     const { addTransaction } = useGlobalState();
-    const [description, setDescription] = useState()
+    const [description, setDescription] = useState("")
     const [amount, setAmount] = useState()
 
     const onSubmit = (e) => {
         e.preventDefault();
-        addTransaction({ 
-            id: window.crypto.randomUUID(), 
-            description, 
-            amount: +amount }) //lo convierte en integer 
+        addTransaction({
+            id: window.crypto.randomUUID(),
+            description,
+            amount: +amount
+        }) //lo convierte en integer 
+        setAmount("");
+        setDescription("")
     }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input type="text" placeholder="Enter a description" onChange={(e) => setDescription(e.target.value)} 
-                className="bg-zinc-600 text-white px-3 py-1 rounded-lg block mb-2 w-full"/>
-                <input type="number" step="0.01" placeholder="00.00" onChange={(e) => setAmount(e.target.value)} 
-                className="bg-zinc-600 text-white px-3 py-1 rounded-lg block mb-2 w-full"/>
+                <input type="text" placeholder="Enter a description"
+                    onChange={(e) => setDescription(e.target.value)}
+                    className="bg-zinc-600 text-white px-3 py-1 rounded-lg block mb-2 w-full" 
+                    value={description}/>
+                <input type="number" step="0.01" placeholder="00.00"
+                    onChange={(e) => setAmount(e.target.value)}
+                    className="bg-zinc-600 text-white px-3 py-1 rounded-lg block mb-2 w-full" 
+                    value={amount}/>
                 <button className="bg-indigo-700 text-white px-3 py-1 rounded-lg block mb-2 w-full ">Add Transaction</button>
             </form>
         </div>
