9b4c008c94d1bbb9d3793d113abaae106a934ee0
DitGrau <juditgraup@gmail.com>
2023-10-18 16:50:42 +0200

    "feat: category and price filter input"

--- reactJournal-03/src/components/Filter.jsx
+++ reactJournal-03/src/components/Filter.jsx
@@ -1,6 +1,22 @@
+import './Filter.css'
+
 export function Filter() {
     return (
-        <>
-        <p>filter</p></>
+
+            <section className="filter">
+                <div>
+                    <label htmlFor='price'>Precio</label>
+                    <input type="range" id='price'
+                        min='0' max='2000' />
+                </div>
+                <div>
+                    <label htmlFor="category">Categoría</label>
+                    <select id="category">
+                        <option value='all'>Todas</option>
+                        <option value='laptops'>Portátiles</option>
+                        <option value='smartphones'>Smartphones</option>
+                    </select>
+                </div>
+            </section>
     )
 }
