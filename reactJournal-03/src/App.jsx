023ffccef6e61d7cbba65130764af5e9f52ef551
DitGrau <juditgraup@gmail.com>
2023-10-18 11:39:21 +0200

    feat: basic filter, by price and category

--- reactJournal-03/src/App.jsx
+++ reactJournal-03/src/App.jsx
@@ -1,12 +1,39 @@
 import { Products } from './components/Products'
-import { products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
+import { useState } from 'react';
 
 function App() {
+
+  const [products] = useState(initialProducts);
+
+  // estado general para los filtros, en lugar de un estado por filtro
+  // filtro por categoria y por precio
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  //filtro: muestra todos los productos que coincidan con el estado inicial o con los nuevos valores asignados
+  //en el parametro le pasamos los products del mock
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      // la sintaxis del .filter es como la del .map
+      return (
+        product.price >= filters.minPrice &&
+        (
+          filters.category === 'all' ||
+          filters.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
 
   return (
     <>
       <h1>Shopping Cart</h1>
-      <Products products={products}/>
+      <Products products={filteredProducts} />
     </>
   )
 }
--- reactJournal-03/src/components/Products.jsx
+++ reactJournal-03/src/components/Products.jsx
@@ -6,7 +6,7 @@ export function Products({ products }) {
         <main className='products'>
             <ul>
                 {
-                    products.map(product => (
+                    products.slice(0,10).map(product => (
                         <li key={product.id}>
                             <img
                                 src={product.thumbnail}
