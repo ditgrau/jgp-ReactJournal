9fe846747b1eb54b9b648bc9fa37d56977016494
DitGrau <juditgraup@gmail.com>
2023-10-19 04:28:02 +0200

    "feat: visual appearance price filter and styles"

--- reactJournal-03/src/components/Filter.jsx
+++ reactJournal-03/src/components/Filter.jsx
@@ -1,22 +1,33 @@
+import { useState } from 'react';
 import './Filter.css'
 
 export function Filter() {
-    return (
 
-            <section className="filter">
-                <div>
-                    <label htmlFor='price'>Precio</label>
-                    <input type="range" id='price'
-                        min='0' max='2000' />
-                </div>
-                <div>
-                    <label htmlFor="category">Categoría</label>
-                    <select id="category">
-                        <option value='all'>Todas</option>
-                        <option value='laptops'>Portátiles</option>
-                        <option value='smartphones'>Smartphones</option>
-                    </select>
-                </div>
-            </section>
+    const [minPrice, setMinPrice] = useState(0);
+
+    const handleChangeMinPrice = (e) => {
+        setMinPrice(e.target.value)
+    }
+
+    return (
+        <section className="filter">
+            <div>
+                <label htmlFor='price'>Precio</label>
+                <input type="range"
+                    id='price'
+                    min='0'
+                    max='2000'
+                    onChange={handleChangeMinPrice} />
+                <span>{minPrice}</span>
+            </div>
+            <div>
+                <label htmlFor="category">Categoría</label>
+                <select id="category">
+                    <option value='all'>Todas</option>
+                    <option value='laptops'>Portátiles</option>
+                    <option value='smartphones'>Smartphones</option>
+                </select>
+            </div>
+        </section>
     )
 }
